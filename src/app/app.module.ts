aafab1eefa91d0d86735f223c5f044d28b24253a
Don McKenzie <don@isyet.com>
2019-05-22 18:10:05 -0500

    Add reducer and connect to app.module. Has some sample 'state'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { SampleDataApi } from './data/sample-data.api';
+
+import { appReducer } from './state/';
 
 import { TestSampleDataComponent } from './data/test-sample-data/test-sample-data.component';
 
@@ -20,7 +25,9 @@ import { TestSampleDataComponent } from './data/test-sample-data/test-sample-dat
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(SampleDataApi),
     AppRoutingModule,
-  ],
+    StoreModule.forRoot({app: appReducer}),
+    StoreDevtoolsModule.instrument({maxAge: 25}),
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
