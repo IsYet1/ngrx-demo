99bc4d6537ab77a6703c0e6d46ed70f6a84c3a5a
Don McKenzie <don@isyet.com>
2019-05-24 10:46:35 -0500

    Add effects file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,10 @@ import { FlexLayoutModule, FlexModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducer } from './state/';
+import { AppEffects } from './state/app.effects';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +20,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { SampleDataApi } from './data/sample-data.api';
 
-import { appReducer } from './state/';
 
 import { TestSampleDataComponent } from './data/test-sample-data/test-sample-data.component';
 import { TestFormComponent } from './test-form/test-form.component';
@@ -41,6 +43,7 @@ import { TestOutputComponent } from './test-output/test-output.component';
     AppRoutingModule,
 
     StoreModule.forRoot({app: appReducer}),
+    EffectsModule.forRoot([AppEffects]),
     StoreDevtoolsModule.instrument({maxAge: 25}),
     ],
   providers: [],
